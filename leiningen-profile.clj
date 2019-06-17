a5fa8465b07f09565cd023ff055ba442eb70dd80
fabian hanselmann <fabian.hanselmann@rewe-digital.com>
2019-06-17 10:44:54 +0200

    added lein kibit instead of kibit library

--- leiningen-profile.clj
+++ leiningen-profile.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.22.0-beta4"]
                   [refactor-nrepl "2.4.0"]
-                  [jonase/kibit "0.1.6"]]}}
+                  [lein-kibit "0.1.6"]]}}
