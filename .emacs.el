00de76f78d964aa03c8959ea93cb9e761691fa79
fabian hanselmann <fabian.hanselmann@rewe-digital.com>
2019-06-18 10:54:33 +0200

    more convenient shortcut for find file and conditional ivy shortcuts

--- .emacs.el
+++ .emacs.el
@@ -242,13 +242,18 @@ There are two things you can do about this warning:
 (global-set-key (kbd "M-1") 'windmove-left)
 (global-set-key (kbd "M-2") 'windmove-right)
 
+;; enable only when projectile is active
+(when (featurep 'projectile)
+  (global-set-key (kbd "M-t") 'project-find-file))
+
 ;; ivy bindings
-(global-set-key (kbd "C-s") 'swiper)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-c i") 'ivy-resume)
-(global-set-key (kbd "C-c m") 'counsel-imenu)
+(when (featurep 'ivy)
+  (global-set-key (kbd "C-s") 'swiper)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (global-set-key (kbd "C-c i") 'ivy-resume)
+  (global-set-key (kbd "C-c m") 'counsel-imenu))
 
 (setq mac-command-modifier 'meta)
 (setq org-log-done t)
