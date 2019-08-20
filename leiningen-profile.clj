a1f477b16b1f23be2f110c5e2927da2257121cb7
fabian.hanselmann@rewe-digital.com <fabian.hanselmann@rewe-digital.com>
2019-08-20 09:22:06 +0200

    using tools.namespace

--- leiningen-profile.clj
+++ leiningen-profile.clj
@@ -1,8 +1,11 @@
-{:user {:plugins [[cider/cider-nrepl "0.22.0-beta8"]
+{:user {:plugins [;[cider/cider-nrepl "0.22.0-beta9"]
                   [lein-kibit "0.1.7"]
                   [lein-eftest "0.5.8"]]
-        :dependencies [[eftest "0.5.8"]]
-        :injections [(intern 'clojure.core 'ppr 'clojure.pprint/pprint)
-                     (intern 'clojure.core 'arrange-requires 'slam.hound/swap-in-reconstructed-ns-form)
+        :dependencies [[eftest "0.5.8"]
+                       [org.clojure/tools.namespace "0.3.1"]]
+        :injections [(require 'clojure.pprint)
+                     (intern 'clojure.core 'ppr clojure.pprint/pprint)
+                     (require 'clojure.tools.namespace.repl)
+                     (intern 'clojure.core 'refresh clojure.tools.namespace.repl/refresh)
                      (refer-clojure)]}}
 
