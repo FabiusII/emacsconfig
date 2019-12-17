f6e8e1c4965bfa10c94a4478b40a05225d6470cf
fabian.hanselmann@rewe-digital.com <fabian.hanselmann@rewe-digital.com>
2019-12-17 17:54:47 +0100

    special characer support in ag search and replace

--- util.el
+++ util.el
@@ -53,9 +53,31 @@ Only the user can detect if the text is actually a reference to the symbol."
   "Opens each file in FILES and asks to replace SYMBOL with REPLACEMENT."
   (when files
     (find-file (car files))
-    (query-replace symbol replacement 'delimited (point-min) (point-max))
+    (goto-char (point-min))
+    (query-replace symbol replacement)
     (save-buffer)
     (util/open-and-replace-in-files (cdr files) symbol replacement)))
+
+(defun util/special-character? (char)
+  "True if CHAR is not a word character."
+  (or (< char 65)
+      (and (> char 90) (< char 97))
+      (> char 122)))
+
+(defun util/escape-characters (str &optional result idx)
+  "Escape all characters with backslash that are not word characters.
+Required by `ag`
+STR the string to escape.
+RESULT The result accumulator for recursion.
+IDX the next character in STR"
+  (let ((idx (or idx 0))
+        (result (or result "")))
+    (if (> (length str) idx)
+        (let ((ascii (aref str idx)))
+          (if (util/special-character? ascii)
+              (util/escape-characters str (concat result (string 92 ascii)) (+ idx 1))
+            (util/escape-characters str (concat result (string ascii)) (+ idx 1))))
+      result)))
 
 (defun rename-in-project ()
   "Renaming a symbol in the current buffer based on its text content alone.
@@ -68,18 +90,21 @@ The latter must be installed on your system, not necessarily EMACS."
 	     (featurep 'subr-x))
     (require 'projectile)
     (require 'subr-x)
-    (let ((old-buffer (current-buffer))
-	  (symbol (thing-at-point 'symbol)))
+    (let ((old-point (point))
+          (old-buffer (current-buffer))
+          (symbol (thing-at-point 'symbol)))
       (when symbol
 	(let* ((prompt (format "replace %s with: " symbol))
 	       (replacement (read-string prompt))
-	       (command (format "ag %s %s" symbol (projectile-project-root)))
+	       (command (format "ag %s %s" (format "\"%s\"" (util/escape-characters symbol)) (projectile-project-root)))
+               (_ (message (format "searching for %s" command)))
 	       (result-string (shell-command-to-string command))
 	       (result-string (string-trim result-string))
 	       (file-list (util/ag-result-string->list result-string)))
 	  (util/open-and-replace-in-files file-list symbol replacement)
 	  (when old-buffer
-	    (switch-to-buffer old-buffer)))))))
+	    (switch-to-buffer old-buffer)
+            (goto-char old-point)))))))
 
 (provide 'util)
 ;;; util.el ends here
