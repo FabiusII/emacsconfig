70718f03236b8482961b8394a396c9208e720557
fabian.hanselmann@rewe-digital.com <fabian.hanselmann@rewe-digital.com>
2020-01-16 14:49:04 +0100

    find and navigate to aliased and imported references

--- util.el
+++ util.el
@@ -1,3 +1,5 @@
+(require 'cl-lib)
+
 (defmacro ->> (&rest body)
   (let ((result (pop body)))
     (dolist (form body result)
@@ -9,3 +11,14 @@
       (setq result (append (list (car form) result)
                            (cdr form))))))
 
+(defun ->hash-table (cons-list)
+  "Takes a list of cons cells and turns it into a hash-table"
+  (cl-reduce (lambda (acc curr)
+                 (if curr
+                     (let* ((key (car curr))
+                            (existing (gethash key acc))
+                            (value (cdr curr)))
+                       (a-assoc acc key (append value existing)))
+                   acc))
+               cons-list
+               :initial-value (a-hash-table)))
